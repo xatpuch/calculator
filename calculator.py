53e746c1d153a7a52a35fe01de853cddc27496df
xatpuch <74716301+xatpuch@users.noreply.github.com>
2021-09-24 00:07:29 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -25,49 +25,47 @@ class Calculator():
         return lex
     def evaluate(self,string):
         x = Calculator.parse(string)
-        stek = []
+        stack = []
         oper = ["+","-","/","*"]
-        stroka = []
+        line = []
         for i in x:
             if i.replace(".","").isdigit():
-                stroka.append(i)
+                line.append(i)
             elif i in oper:
-                if stek == []:
-                    stek.append(i)
-                elif Calculator.pr(stek[-1]) < Calculator.pr(i):
-                    stek.append(i)
-                elif Calculator.pr(stek[-1]) >= Calculator.pr(i):
-                    while stek != [] and Calculator.pr(stek[-1]) >= Calculator.pr(i):
-                        stroka.append(stek.pop(-1))
-                    stek.append(i)
+                if stack == []:
+                    stack.append(i)
+                elif Calculator.pr(stack[-1]) < Calculator.pr(i):
+                    stack.append(i)
+                elif Calculator.pr(stack[-1]) >= Calculator.pr(i):
+                    while stack != [] and Calculator.pr(stack[-1]) >= Calculator.pr(i):
+                        line.append(stack.pop(-1))
+                    stack.append(i)
             elif i == "(":
-                stek.append(i)
+                stack.append(i)
             elif i == ")":
-                for i in stek[::-1]:
+                for i in stack[::-1]:
                     if i != "(":
-                        stroka.append(stek.pop())
+                        line.append(stack.pop())
                     else:
-                        stek.pop(-1)
+                        stack.pop(-1)
                         break
-        #print(stroka,stek)
-        pol = stroka+stek[::-1]
-        #return pol
-        stek2 = []
+        pol = line+stack[::-1]
+        stack2 = []
         for i in pol:
             if i.replace(".","").isdigit():
-                stek2.append(i)
+                stack2.append(i)
             elif i == "+" or i == "-" or i == "/" or i == "*":
-                if i == "+" and stek2 != []:
-                    stek2.append(float(stek2.pop())+float(stek2.pop()))
-                elif i == "*" and stek2 != []:
-                    stek2.append(float(stek2.pop())*float(stek2.pop()))
-                elif i == "-" and stek2 != []:
-                    stek2.append(float(stek2.pop(-2))-float(stek2.pop()))
-                elif i == "/" and stek2 != []:
-                    stek2.append(float(stek2.pop(-2))/float(stek2.pop()))
-        if int(stek2[0]) == stek2[0]:
-            return stek2[0]
-        elif type(stek2[0]) == float:
-            return stek2[0]
+                if i == "+" and stack2 != []:
+                    stack2.append(float(stack2.pop())+float(stack2.pop()))
+                elif i == "*" and stack2 != []:
+                    stack2.append(float(stack2.pop())*float(stack2.pop()))
+                elif i == "-" and stack2 != []:
+                    stack2.append(float(stack2.pop(-2))-float(stack2.pop()))
+                elif i == "/" and stack2 != []:
+                    stack2.append(float(stack2.pop(-2))/float(stack2.pop()))
+        if int(stack2[0]) == stack2[0]:
+            return stack2[0]
+        elif type(stack2[0]) == float:
+            return stack2[0]
         else:
-            return int(stek2[0])
+            return int(stack2[0])
